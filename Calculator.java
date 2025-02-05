8a4940a3b533bbc6c220c0aeb251e350adc172b2
Emile Cappelle <emilecappelle@icloud.com>
2025-02-05 19:21:17 +0100

    Calculator

---
+++ Calculator.java
@@ -0,0 +1,34 @@
+
+/**
+ * class Calculator - geef hier een beschrijving van deze class
+ *
+ * @author Emile Cappelle
+ * @version 2025-02-05
+ */
+public class Calculator
+{
+    public Calculator()
+    {
+        
+    }
+    
+    public double add(double a, double b)
+    {
+        return a + b;
+    }
+    
+    public double substract(double a, double b)
+    {
+        return a - b;
+    }
+    
+    public double multiply(double a, double b)
+    {
+        return a * b;
+    }
+    
+    public double divide(double a, double b)
+    {
+        return a / b;
+    }
+}
